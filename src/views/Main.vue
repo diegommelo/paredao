3bbb15b4d61e390e69ef4d891fa121f7f33babce
diego <diego.jackchan@gmail.com>
2020-01-09 12:55:19 -0200

    fotos completas

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -157,7 +157,7 @@ export default {
       restantes:[],
       campeao:[],
       historico:[],
-      edicoes_totais:[1,2,3,4,5,6,7,8,9,10,11,12,13,14],
+      edicoes_totais:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],
       edicoes_escolhidas:[],
       participantes: 14,
       showcard1:true,
