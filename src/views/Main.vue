3980c06b1c18bf592689ed02097ef64b66721544
diego <diego.jackchan@gmail.com>
2020-01-22 10:25:21 -0200

    adicionado participantes bbb20

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -175,7 +175,7 @@ export default {
       restantes:[],
       campeao:[],
       historico:[],
-      edicoes_totais:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],
+      edicoes_totais:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
       edicoes_escolhidas:[],
       participantes: 14,
       showcard1:true,
